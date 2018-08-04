be7467bd77676f59616f944375d0d1c598f0947d
John Landi <salaino777@gmail.com>
2018-08-04 21:01:33 +0900

    one player can move and spin

--- SpinDogtor/Assets/Scripts/PlayerMove.cs
+++ SpinDogtor/Assets/Scripts/PlayerMove.cs
@@ -4,13 +4,63 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //movespeed for character float
+    public float moveSpeed;
+
+    private float vertTranslation;
+    private float horiTranslation;
+
+    private float rx;
+    private float ry;
+    private float angle;
+    public float rotationSpeed;
+
+    private Rigidbody2D rb2D;
+
+    // Use this for initialization
+    void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate() {
+
+        vertTranslation = Input.GetAxis("Vertical") * moveSpeed;
+        horiTranslation = Input.GetAxis("Horizontal") * moveSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        vertTranslation *= Time.deltaTime;
+        horiTranslation *= Time.deltaTime;
+
+
+
+        // Move translation along the object's z-axis
+        //transform.Translate(horiTranslation, vertTranslation, 0, Space.World);
+
+
+        rb2D.AddForce(new Vector2(horiTranslation, vertTranslation ) * moveSpeed);
+
+        rx = Input.GetAxis("Horizontal2");
+        ry = Input.GetAxis("Vertical2");
+
+        float angle = Mathf.Atan2(-rx, ry) * Mathf.Rad2Deg;
+
+        Debug.Log(angle);
+
+
+        if (rx != 0 || ry != 0)
+            rb2D.MoveRotation(angle);
+
+        /* float angle = Mathf.Atan2(-rx, ry);
+
+         if (rx != 0 || ry != 0)
+         {
+             //rb2D.AddTorque(
+
+             transform.rotation = Quaternion.EulerAngles(0, 0, angle);
+         }
+
+     */
+
+    }
 }
